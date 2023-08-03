4dacc36f308c231a942f6bccb2be311d081de81f
Adnan Amin <geoamins@gmail.com>
2023-08-03 14:18:48 +0500

    proj

---
+++ documentation/laravel_class/myfirstapp/resources/views/layout/scripts.blade.php
@@ -0,0 +1,12 @@
+<!-- Script -->
+<script src="{{ asset('assets/js/jquery-1.12.4.min.js') }}"></script>
+<script src="{{ asset('assets/js/isotope.pkg.min.js') }}"></script>
+<script src="{{ asset('assets/js/wow.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.slick.min.js') }}"></script>
+<script src="{{ asset('assets/js/lightgallery.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.counter.min.js') }}"></script>
+<script src="{{ asset('assets/js/select2.min.js') }}"></script>
+<script src="{{ asset('assets/js/ripples.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('assets/js/animated-headline.js') }}"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
---
+++ documentation/laravel_class/myfirstapp/resources/views/layout/style.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Laralink">
+    <!-- Site Title -->
+    <title>Insurance Home</title>
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/lightgallery.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/select2.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/slick.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/jquery-ui.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animated-headline.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+</head>
