856e0e6a3e5eabee18965a409dda133ace50dda1
Rizwan Sarwar <iamrizwansarwar@gmail.com>
2023-08-03 13:25:50 +0500

    Update Login Title
    Update Login Title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Login Form | School Management System</title>
+  <title>Rizwan Sarwar | School Management System</title>
 
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css">
