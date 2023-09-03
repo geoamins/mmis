6d0296c74cda7e29d1ab5ea6235aaad9d2d03a87
Rizwan Sarwar <iamrizwansarwar@gmail.com>
2023-09-03 15:01:10 +0500

    Student Show Modified
    Show View Modified

--- storage/framework/views/68eb4d9b14f36ec534554947d5ef67e1.php
+++ storage/framework/views/68eb4d9b14f36ec534554947d5ef67e1.php
@@ -14,14 +14,12 @@
         .profile{
             display: flex;
             width: 100%;
-            /* height: 300px; */
             margin-top: 20px;
         }
         .profile .left{
             text-align: center;
             width: 10%;
             height: 135px;
-            /* border: 1px solid black; */
             margin: 20px;
         }
         .left img{
@@ -41,6 +39,9 @@
         .profile .right .rleft{
             width: 50%;
         }
+        .profile .right .rright{
+            width: 50%;
+        }
         .rleft p{
             margin-bottom: 15px;
         }
@@ -51,14 +52,12 @@
         .admissiondetail{
             display: flex;
             width: 100%;
-            /* height: 300px; */
             margin-top: 20px;
         }
         .admissiondetail .left{
             text-align: center;
             width: 10%;
             height: 150px;
-            /* border: 1px solid black; */
             margin: 20px;
         }
         .admissiondetail .right{
@@ -69,6 +68,9 @@
         }
 
         .admissiondetail .right .rleft{
+            width: 50%;
+        }
+        .admissiondetail .right .rright{
             width: 50%;
         }
         .admissiondetail .rleft p{
@@ -82,14 +84,12 @@
         .guardiandetail{
             display: flex;
             width: 100%;
-            /* height: 300px; */
             margin-top: 20px;
         }
         .guardiandetail .left{
             text-align: center;
             width: 10%;
             height: 150px;
-            /* border: 1px solid black; */
             margin: 20px;
         }
         .guardiandetail .right{
@@ -100,6 +100,9 @@
         }
 
         .guardiandetail .right .rleft{
+            width: 50%;
+        }
+        .guardiandetail .right .rright{
             width: 50%;
         }
         .guardiandetail .rleft p{
@@ -119,7 +122,6 @@
             text-align: center;
             height: 150px;
             width: 10%;
-            /* border: 1px solid black; */
             margin: 20px;
         }
         .otherdetail .right{
@@ -130,6 +132,9 @@
         }
 
         .otherdetail .right .rleft{
+            width: 50%;
+        }
+        .otherdetail .right .rright{
             width: 50%;
         }
         .otherdetail .rleft p{
@@ -156,6 +161,19 @@
         .btnheader button a{
             color: white;
         }
+        .data{
+            width: 75%;
+            height: 38px;
+            padding: 8px;
+            border: 1px solid black;
+            border-radius: 3px;
+            margin-bottom: 6px;
+            background-color: rgb(239, 239, 239);
+        }
+
+        .data p{
+            font-weight: bold;
+        }
     </style>
 </head>
 
@@ -177,39 +195,69 @@
             <div class="right">
                 <div class="rleft">
                    <h5><?php echo e(__('student.Registration No')); ?></h5>
-                    <p><?php echo e($data->RegistrationNo); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->RegistrationNo); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.CNIC')); ?></h5>
-                    <p><?php echo e($data->SCNIC); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->SCNIC); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Gender')); ?></h5>
                     <?php if($data->GenderID == 1): ?>
-                        <p><?php echo e(__('student.Male')); ?></p>
+                        <div class="data">
+                            <p><?php echo e(__('student.Male')); ?></p>
+                        </div>
                     <?php elseif($data->GenderID == 2): ?>
-                        <p><?php echo e(__('student.Female')); ?></p>
+                        <div class="data">
+                            <p><?php echo e(__('student.Female')); ?></p>
+                        </div>
                     <?php endif; ?>
                     <h5><?php echo e(__('student.Father Name')); ?></h5>
-                    <p><?php echo e($data->FatherName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->FatherName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Father Mobile')); ?></h5>
-                    <p><?php echo e($data->FMobile); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->FMobile); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Permanent Address')); ?></h5>
-                    <p><?php echo e($data->PermanentAddress); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->PermanentAddress); ?></p>
+                    </div>
                     <h5><?php echo e(__('Basic.Province Name')); ?></h5>
-                    <p><?php echo e($data->ProvinceName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->ProvinceName); ?></p>
+                    </div>
                 </div>
                 <div class="rright">
                     <h5><?php echo e(__('student.Full Name')); ?></h5>
-                    <p><?php echo e($data->StudentName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->StudentName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.DOB')); ?></h5>
-                    <p><?php echo e($data->DOB); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->DOB); ?></p>
+                    </div>
                     <h5><?php echo e(__('Basic.Department Name')); ?></h5>
-                    <p><?php echo e($data->DepartmentName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->DepartmentName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Father CNIC')); ?></h5>
-                    <p><?php echo e($data->FCNIC); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->FCNIC); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Current Address')); ?></h5>
-                    <p><?php echo e($data->CurrentAddress); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->CurrentAddress); ?></p>
+                    </div>
                     <h5><?php echo e(__('Basic.Country Name')); ?></h5>
-                    <p><?php echo e($data->CountryName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->CountryName); ?></p>
+                    </div>
                     <h5><?php echo e(__('Basic.District Name')); ?></h5>
-                    <p><?php echo e($data->DistrictName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->DistrictName); ?></p>
+                    </div>
 
 
 
@@ -226,33 +274,57 @@
             <div class="right">
                 <div class="rleft">
                     <h5><?php echo e(__('student.Admission Session')); ?></h5>
-                    <p><?php echo e($data->SessionTitle); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->SessionTitle); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Hajri Year')); ?></h5>
-                    <p><?php echo e($data->HijriYear); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->HijriYear); ?></p>
+                    </div>
                     <h5><?php echo e(__('Basic.Class Name')); ?></h5>
-                    <p><?php echo e($data->ClassName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->ClassName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Previous Madrassa')); ?></h5>
-                    <p><?php echo e($data->PreviousMadrasa); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->PreviousMadrasa); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Asri Education')); ?></h5>
-                    <p><?php echo e($data->AsriEdu); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->AsriEdu); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Hostel Status')); ?></h5>
                     <?php if($data->HostelStatus == 1): ?>
-                        <p><?php echo e(__('student.Resident')); ?></p>
+                        <div class="data">
+                            <p><?php echo e(__('student.Resident')); ?></p>
+                        </div>
                     <?php elseif($data->HostelStatus == 0): ?>
-                        <p><?php echo e(__('student.Non Resident')); ?></p>
+                        <div class="data">
+                            <p><?php echo e(__('student.Non Resident')); ?></p>
+                        </div>
                     <?php endif; ?>
                 </div>
                 <div class="rright">
                     <h5><?php echo e(__('student.Admission Date')); ?></h5>
-                    <p><?php echo e($data->AdmissionDate); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->AdmissionDate); ?></p>
+                    </div>
                     <h5><?php echo e(__('Basic.Student Type')); ?></h5>
-                    <p><?php echo e($data->StudentType); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->StudentType); ?></p>
+                    </div>
                     <h5><?php echo e(__('Basic.Section Name')); ?></h5>
-                    <p><?php echo e($data->SectionName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->SectionName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Islamic Education')); ?></h5>
-                    <p><?php echo e($data->IslamicEdu); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->IslamicEdu); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Additional Education')); ?></h5>
+                   <div class="data">
                     <p><?php echo e($data->AddlEdu); ?></p>
+                   </div>
 
                 </div>
             </div>
@@ -265,15 +337,23 @@
             <div class="right">
                 <div class="rleft">
                     <h5><?php echo e(__('student.Father Name')); ?></h5>
-                    <p><?php echo e($data->FatherName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->FatherName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Guardian Name')); ?></h5>
-                    <p><?php echo e($data->GuardianName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->GuardianName); ?></p>
+                    </div>
                 </div>
                 <div class="rright">
                     <h5><?php echo e(__('student.Father CNIC')); ?></h5>
+                   <div class="data">
                     <p><?php echo e($data->FCNIC); ?></p>
+                   </div>
                     <h5><?php echo e(__('student.Guardian Relation')); ?></h5>
-                    <p><?php echo e($data->GuardianRelation); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->GuardianRelation); ?></p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -285,19 +365,31 @@
             <div class="right">
                 <div class="rleft">
                     <h5><?php echo e(__('student.Monthly Fee')); ?></h5>
-                    <p><?php echo e($data->StudentName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->StudentName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Fee Discount')); ?></h5>
-                    <p><?php echo e($data->StudentName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->StudentName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Struck Off Date')); ?></h5>
-                    <p><?php echo e($data->DOSLC); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->DOSLC); ?></p>
+                    </div>
                 </div>
                 <div class="rright">
                     <h5><?php echo e(__('student.Attached brother')); ?></h5>
-                    <p><?php echo e($data->StudentName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->StudentName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Total Fee')); ?></h5>
-                    <p><?php echo e($data->StudentName); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->StudentName); ?></p>
+                    </div>
                     <h5><?php echo e(__('student.Struck Off Reason')); ?></h5>
-                    <p><?php echo e($data->ReasonSLC); ?></p>
+                    <div class="data">
+                        <p><?php echo e($data->ReasonSLC); ?></p>
+                    </div>
                 </div>
             </div>
         </div>
